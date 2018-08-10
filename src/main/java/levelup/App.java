867653638240fedc6cca5d084bf3bdd200879d93
Sark90 <mmg9658@gmail.com>
2018-08-10 13:31:51 +0300

    1. Reading from properties + custom properties editor

--- src/main/java/levelup/App.java
+++ src/main/java/levelup/App.java
@@ -4,13 +4,25 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
         GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
         ctx.load("classpath:META-INF/spring/app-context.xml");
         ctx.refresh();
 
         User user = ctx.getBean("user", User.class);
+        showUserInfo(user);
+        System.out.println();
+
+/*        user.setName("James");
+        user.setSurname("Bond");
+        DateEditor dEdit = ctx.getBean("editor", DateEditor.class);
+        dEdit.setAsText("06-01-1974");
+        //user.setBirthday(dEdit.getValue());
+        showUserInfo(user);*/
+    }
+
+    private static void showUserInfo(User user) {
         System.out.println("User info\nname: " + user.getName() + "\nsurname: " + user.getSurname()
                 + "\nbirthday: " + user.getBirthday().toString());
     }
